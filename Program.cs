9f6e3b191a6fb57bcc7b68a7309cee581b39d483
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-09-10 14:52:14 -0500

    updated Main method to create new instance of RoomRepository; updated code inside while loop to invoke GetAll() method to display all rooms

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using Roommates.Repositories;
+using Roommates.Models;
 
 namespace Roommates
 {
@@ -11,6 +13,8 @@ namespace Roommates
 
         static void Main(string[] args)
         {
+            RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING); // instantiating new RoomRepository
+
             bool runProgram = true;
             while (runProgram)
             {
@@ -19,7 +23,13 @@ namespace Roommates
                 switch (selection)
                 {
                     case ("Show all rooms"):
-                        // Do stuff
+                        List<Room> rooms = roomRepo.GetAll();
+                        foreach (Room r in rooms)
+                        {
+                            Console.WriteLine($"{r.Name} has an Id of {r.Id} and a max occupancy of {r.MaxOccupancy}");
+                        }
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
                         break;
                     case ("Search for room"):
                         // Do stuff
